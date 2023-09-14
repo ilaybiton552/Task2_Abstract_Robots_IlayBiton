1fd52d0aa3ea687f2bd7f92451a804e4d84ae573
ilaybiton552 <ilaybitonminecraft@gmail.com>
2023-09-14 10:29:32 +0300

    Finish RobotQuad

--- Abstract_Robots/Robots/RobotQuad.cs
+++ Abstract_Robots/Robots/RobotQuad.cs
@@ -8,9 +8,13 @@ namespace Robots_inc
 {
     public class RobotQuad : RobotSpy
     {
-        //1. עדכנו את הפעולה הבונה כך שתתאים לפעולת במחלקת העל
+        private int[] legs; 
+        // 0 - left front, 1 - right front, 2 - left back, 3 - right back
+
         public RobotQuad() : base("Roboquad")
-        { }
+        { 
+            legs = new int[4];
+        }
 
         public override void MoveForward()
         {
@@ -18,11 +22,32 @@ namespace Robots_inc
                 this.MoveLeg(i, 1);
         }
 
-        //2. השלימו את התנועות החסרות מתוך המצגת או על הבנתכם
+        public override void MoveBackward()
+        {
+            for (int i = 0; i < 4; i++)
+                this.MoveLeg(i, -1);
+        }
+
+        public override void TurnLeft()
+        {
+            MoveLeg(0, -1);
+            MoveLeg(1, 1);
+            MoveLeg(2, -1);
+            MoveLeg(3, 1);
+        }
+
+        public override void TurnRight()
+        {
+            MoveLeg(0, 1);
+            MoveLeg(1, -1);
+            MoveLeg(2, 1);
+            MoveLeg(3, -1);
+        }
 
         private void MoveLeg(int legId, int dir) 
         {
-            //3. הוסיפו התייחסות לסוללה
+            SetBatteryStatus(GetBatteryStatus() - 3);
+            legs[legId] += dir;
         }
     }
 
